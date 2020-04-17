2f2f09a3fcaf1042313edcda10d20d7efe33939d
Pat Coleman <padsterpat@gmail.com>
2020-04-17 13:31:44 -0700

    Updated notebooks for different sections of code

--- project/botSrc/CPSC533V/src/bot.py
+++ project/botSrc/CPSC533V/src/bot.py
@@ -1,125 +1,134 @@
+# RLBot code, for an interface between RLBot state/action and our
+#       own trained agent's pytorch state/actions
+
+# PARAMETERS
+USE_GPU = False
+BC_MODEL = "bc_boostOnly1v1_AI3.BN"
+DQN_MODEL = None # "dqn.2layer.BN.SandARewards"
+assert ((BC_MODEL is None) ^ (DQN_MODEL is None)), "Need either a BC or DQN model"
+
 import math
-import pickle
-
-from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
-from rlbot.utils.structures.game_data_struct import GameTickPacket
-
-from util.orientation import Orientation
-from util.vec import Vec3
-
 import numpy as np
+import pickle
+import os
+import sys
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+DEVICE = torch.device("cuda" if USE_GPU else "cpu")
 
-print ("\n\n****\nLOADING\n\n")
-STATE_DICT = torch.load("C:/Users/User/code/CPSC533V/project/models/boostOnly1v1.pt")
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+from util.orientation import Orientation
+from util.vec import Vec3
+
+ROOT_PROJECT_PATH = 'C:/Users/User/code/CPSC533V/project'
+sys.path.append(ROOT_PROJECT_PATH)
+import lib.preprocess as libPreprocess
+import lib.rewards as libRewards
+from lib.SAtoVModel import SAtoV_Model
+from lib.StoAModel import StoA_Model
+
+# Load the model now: Note we can't load it while the bot is running, too slow.
+print ("\n\n****\nLOADING pytorch model...\n\n")
+if BC_MODEL is not None:
+    STATE_DICT = torch.load(os.path.join(ROOT_PROJECT_PATH, "models", "%s.pt" % BC_MODEL))
+elif DQN_MODEL is not None:
+    STATE_DICT = torch.load(os.path.join(ROOT_PROJECT_PATH, "models", "%s.pt" % DQN_MODEL))
 print ("\n\nLOADED!\n")
 
-def add_state_vec3(state, vec3):
-    state.append(vec3.x)
-    state.append(vec3.y)
-    state.append(vec3.z)
 
-def add_state_phys(state, phys):
-    add_state_vec3(state, phys.location) # pos
-    #state.append(phys.velocity.pitch)
-    #state.append(phys.velocity.yaw)
-    #state.append(phys.velocity.roll)
-    add_state_vec3(state, phys.velocity) # vel
-    #add_state_vec3(state, phys.rotation) # rot
-    state.append(phys.rotation.pitch)
-    state.append(phys.rotation.yaw)
-    state.append(phys.rotation.roll)
-    add_state_vec3(state, phys.angular_velocity) # ang_vel
+# Adding game state to a pytorch state tensor:
+def _normMiddlePeak(v):
+    return 4/(1 + np.exp(-v)) - 2
 
+def add_ball_state(state, phys):
+    state.append(phys.location.x / libPreprocess.POS_X_MAX)
+    state.append(phys.location.y / libPreprocess.POS_Y_MAX)
+    state.append(phys.location.z / libPreprocess.POS_Z_MAX)
 
-## HACK - model, should be shared with notebook and trainer.
-class MyModel(nn.Module):
-    def __init__(self, state_size, action_analog_size, action_digital_size):
-        super(MyModel, self).__init__()
-        
-        # Start with a super simple multi-layer perceptron, one hidden layer 
-        self.dimH          = 32 # hidden layer has 16 dimensions
-        self.dimIn         = state_size
-        self.dimOutAnalog  = action_analog_size
-        self.dimOutDigital = action_digital_size
-        self.dimOut = action_analog_size + action_digital_size
-     
-        self.model = torch.nn.Sequential(
-           nn.Linear(self.dimIn, self.dimH),
-           nn.ReLU(),
-           nn.Linear(self.dimH, self.dimOut),
-        )
-        #self.model.to(device)
+def add_player_state(state, phys, boost):
+    # Note: Need to keep in sync with libPreprocess
+    state.append(phys.location.x / libPreprocess.POS_X_MAX)
+    state.append(phys.location.y / libPreprocess.POS_Y_MAX)
+    state.append(phys.location.z / libPreprocess.POS_Z_MAX)
+    state.append(_normMiddlePeak(phys.velocity.x / libPreprocess.VEL_X_MAX))
+    state.append(_normMiddlePeak(phys.velocity.y / libPreprocess.VEL_Y_MAX))
+    state.append(_normMiddlePeak(_normMiddlePeak(phys.velocity.z / libPreprocess.VEL_Z_MAX)))
+    state.append(_normMiddlePeak(phys.rotation.pitch / (np.pi / 2)))
+    state.append(phys.rotation.yaw / np.pi)
+    state.append(_normMiddlePeak(phys.rotation.roll / np.pi))
+    state.append(phys.angular_velocity.x / libPreprocess.ANG_VEL_MAX)
+    state.append(phys.angular_velocity.y / libPreprocess.ANG_VEL_MAX)
+    state.append(phys.angular_velocity.z / libPreprocess.ANG_VEL_MAX)
+    state.append(boost / libPreprocess.BOOST_MAX)
 
-    def forward(self, x):
-        # input data type needs to be converted to float
-        return self.model(x.float())
-        
-    def save(self, modelID):
-        path = os.path.join("models", "%s.pt" % modelID)
-        torch.save(self.state_dict(), path)
-        print('Saved model!\n\t%s' % path)
-        
-    def load(self, modelID):
-        path = "C:/Users/User/code/CPSC533V/project/models/%s.pt" % modelID
-        self.load_state_dict(STATE_DICT)
-        print('Loaded model!\n\t%s' % path)
-
-
+# Use RLBot's debugging tool to print the action output above the car
 def debugTxt(R, car, msg):
-    print (msg)
     R.begin_rendering()
-    # print the action that the bot is taking
     R.draw_string_3d(car.physics.location, 2, 2, msg, R.white())
     R.end_rendering()
 
-def clip(lo, x, hi):
-    return max(lo, min(x, hi))
+# Sigmoid in numpy rather than pytorch
+def npSigmoid(X):
+    return 1/(1+np.exp(-X))
 
-# BOT
+# Actual bot code starts here.
 class MyBot(BaseAgent):
-
     def initialize_agent(self):
         print( "\n\n\n***\n\nCREATING BOT\n\n***\n\n")
-
         # This runs once before the bot starts up
         self.controller_state = SimpleControllerState()
 
         nPerTeam = 1
         self.stateDim = 3 + 2 * nPerTeam * (4*3 + 1)
-        self.model = MyModel(self.stateDim, 2, 1)
-        self.model.load("boostOnly1v1")
+        if BC_MODEL is not None:
+            self.model = StoA_Model(self.stateDim, 3, DEVICE)
+        else:
+            self.model = SAtoV_Model(self.stateDim, 3, DEVICE)
+        self.model.load(STATE_DICT)
+        self.model.eval()
         print ("Model loaded!")
 
-    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+    # Create state tensor from RLBot state:
+    def packet_to_state_tensor(self, packet: GameTickPacket):
         state = []
+        add_ball_state(state, packet.game_ball.physics) # b_
+        my_car = packet.game_cars[self.index]
+        enemy_car = packet.game_cars[1 - self.index]
+        add_player_state(state, my_car.physics, my_car.boost) # me_
+        add_player_state(state, enemy_car.physics, enemy_car.boost) # e0_
+        assert (len(state) == self.stateDim)
+        return torch.from_numpy(np.array(state)).float().unsqueeze(0)
 
-        ball_location = packet.game_ball.physics.location
-        add_state_vec3(state, ball_location) # b_pos
+    # Convert game state to pytorch state to model actions to game actions
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        # Output actions
+        aThrottle, aSteer, aBoost = 0, 0, False
+
+        state_tensor = self.packet_to_state_tensor(packet)
+        if BC_MODEL is not None:
+            # BC, so use model to predict A from Q
+            action_tensor = self.model(state_tensor).squeeze()
+            action = action_tensor.detach().numpy()
+            assert (len(action) == 3)
+
+            # And convert A into two [-1, 1] and one boolean
+            aAnalog = 2 * npSigmoid(action[:2]) - 1
+            aThrottle, aSteer, aBoost = aAnalog[0], aAnalog[1], (action[2] > 0)
+
+        else:
+            # DQN, so find best action by using many to get the best Q:
+            _, aMax = libRewards.bestQ(self.model, state_tensor, returnAction=True)
+            (aThrottle, aSteer, aBoost) = aMax
 
         my_car = packet.game_cars[self.index]
-        add_state_phys(state, my_car.physics) # me_
-        state.append(my_car.boost)
-
-        enemy_car = packet.game_cars[1 - self.index]
-        add_state_phys(state, enemy_car.physics) # e0_
-        state.append(enemy_car.boost)
-        assert (len(state) == self.stateDim)
-
-        state_tensor = torch.from_numpy(np.array(state)).float()
-
-        action_tensor = self.model(state_tensor)
-        action = action_tensor.detach().numpy()
-        assert (len(action) == 3)
-        debugTxt(self.renderer, my_car, "(%.2f %.2f %.2f)" % (action[0], action[1], action[2]))
+        #debugTxt(self.renderer, my_car, "(%.2f %.2f %.2f)" % (aThrottle, aSteer, float(aBoost)))
+        debugTxt(self.renderer, my_car, "(%.2f %.2f %.2f)" % (state_tensor[0][16], state_tensor[0][17], state_tensor[0][18]))
 
         # throttle, steer, boost
-        self.controller_state.throttle = clip(-1, action[0], 1)
-        self.controller_state.steer = clip(-1, action[1], 1)
-        self.controller_state.boost = (action[2] > 0)
-
+        self.controller_state.throttle = aThrottle
+        self.controller_state.steer = aSteer
+        self.controller_state.boost = aBoost
         return (self.controller_state)
-
-
